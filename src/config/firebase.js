f3c84a99f2ef2a0c98d653bd8ecf884c1fe6ab27
Dmitry Levanov <mitrskoggson@yandex.ru>
2019-03-12 16:53:02 +0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,8 @@
 export default {
     apiKey: "AIzaSyCU2ygvoj9kIfOcqmxdfyeYFMy_-NE7Ar8",
-    authDomain: "serviceapp-42ff7.firebaseapp.com",
-    databaseURL: "https://serviceapp-42ff7.firebaseio.com",
-    projectId: "serviceapp-42ff7",
-    storageBucket: "serviceapp-42ff7.appspot.com",
+    authDomain: "xxx.firebaseapp.com",
+    databaseURL: "https://xxx.firebaseio.com",
+    projectId: "x",
+    storageBucket: "x.appspot.com",
     messagingSenderId: "643333224361"
 };
